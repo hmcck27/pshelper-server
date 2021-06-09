7e5cc20b03a9c247c44054c5dd46d7fde4017e1f
backend <backend@JK.local>
2021-06-09 20:32:18 +0900

    3

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 ''' dir path for koBERT+CNN model inference in jk-local'''
 model_path = '/Users/backend/PycharmProjects/pshelper-server/model/kobert_from_pretrained'
-cnn_path = '/Users/backend/PycharmProjects/pshelper-server/model/freeze_cnn.pt'
+# cnn_path = '/Users/backend/PycharmProjects/pshelper-server/model/freeze_cnn.pt'
+cnn_path = '/Users/backend/PycharmProjects/pshelper-server/model/cnn_dict.pt'
 vocab_path = '/Users/backend/PycharmProjects/pshelper-server/model/kobert_news_wiki_ko_cased-1087f8699e.spiece'
 
 ''' dir path for koBERT+CNN model inference in pshelper-server'''
